ccd332be9db0b045f853ff0553306f87da4692c0
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-26 14:21:30 +0300

    Update Automate Google Calendar api.py

--- Automate Google Calendar api.py
+++ Automate Google Calendar api.py
@@ -3,4 +3,16 @@ import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+# Set up the necessary credentials and authorization
+creds, _ = google.auth.default()
+service = build('calendar', 'v3', credentials=creds)
 
+# Define the event details
+event = {
+  'summary': 'Test Event',
+  'location': '123 Main Street',
+  'description': 'This is a test event.',
+  'start': {
+    'dateTime': '2022-01-01T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+ 
