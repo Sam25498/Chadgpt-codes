5101d53e3e8dc534d2138ad430e6c7596540a7ec
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-28 16:22:05 +0300

    Update Double tops and double-bottoms trading view detector.py

--- Double tops and double-bottoms trading view detector.py
+++ Double tops and double-bottoms trading view detector.py
@@ -12,3 +12,16 @@ df = pd.DataFrame(data, columns=["timestamp", "open", "high", "low", "close", "v
 # For double tops, we will look for two consecutive highs that are relatively close to each other
 # For double bottoms, we will look for two consecutive lows that are relatively close to each other
 
+# To do this, we can define a threshold distance (in pips) that we will use to determine if two highs or lows are "close"
+threshold_distance = 20
+
+# Now, we can loop through the data and check for double tops and double bottoms
+for i in range(1, len(df) - 1):
+    # Check for double tops
+    if (df.iloc[i]["high"] - df.iloc[i-1]["high"] < threshold_distance) and (df.iloc[i+1]["high"] - df.iloc[i]["high"] < threshold_distance):
+        print("Double top detected at", df.iloc[i]["timestamp"])
+
+    # Check for double bottoms
+    if (df.iloc[i-1]["low"] - df.iloc[i]["low"] < threshold_distance) and (df.iloc[i]["low"] - df.iloc[i+1]["low"] < threshold_distance):
+        print("Double bottom detected at", df.iloc[i]["timestamp"])
+
