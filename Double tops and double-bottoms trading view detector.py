3d9d0dd2ced284f945020977495574d979e84f15
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-28 16:21:34 +0300

    Update Double tops and double-bottoms trading view detector.py

--- Double tops and double-bottoms trading view detector.py
+++ Double tops and double-bottoms trading view detector.py
@@ -5,3 +5,10 @@ import pandas as pd
 # Next, we need to retrieve the data for the EURUSD 1 hour chart
 data = tv.fetch_ohlcv("EURUSD", "1h")
 
+# Now, we can convert the data into a pandas DataFrame for easier analysis
+df = pd.DataFrame(data, columns=["timestamp", "open", "high", "low", "close", "volume"])
+
+# We can then use the 'high' and 'low' columns to detect double tops and double bottoms
+# For double tops, we will look for two consecutive highs that are relatively close to each other
+# For double bottoms, we will look for two consecutive lows that are relatively close to each other
+
