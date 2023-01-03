b02c4b8ae63bcbc580d67303990a7142a7688f43
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-03 22:24:41 +0300

    Update Momentum based strategy.py

--- Momentum based strategy.py
+++ Momentum based strategy.py
@@ -4,3 +4,15 @@ import numpy as np
 # Load the data
 df = pd.read_csv("EURUSD_4h.csv")
 
+# Convert the date strings to datetime objects
+df['Date'] = pd.to_datetime(df['Date'])
+
+# Set the datetime column as the index
+df.set_index('Date', inplace=True)
+
+# Calculate the momentum
+df['Momentum'] = df['Close'].diff(periods=12)
+
+# Buy when the momentum is positive
+df['Position'] = np.where(df['Momentum'] > 0, 1, 0)
+
