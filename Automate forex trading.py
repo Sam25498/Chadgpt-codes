f410efd47bd4110a2670be53baa9d42cedac90d4
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-05 00:00:02 +0300

    Update Automate forex trading.py

--- Automate forex trading.py
+++ Automate forex trading.py
@@ -3,3 +3,11 @@ from forex_python.converter import CurrencyRates
 c = CurrencyRates()
 price = c.get_rate('USD', 'EUR')
 print(price)
+import time
+
+while True:
+    # Retrieve the current price
+    price = c.get_rate('USD', 'EUR')
+    print(f'Current price: {price}')
+
+
