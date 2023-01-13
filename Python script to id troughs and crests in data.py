29ff409492d8b09cc2df9f4bbc7e0a61ed60d0b8
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-13 15:07:33 +0300

    Update Python script to id troughs and crests in data.py

--- Python script to id troughs and crests in data.py
+++ Python script to id troughs and crests in data.py
@@ -4,3 +4,11 @@ def identify_troughs(forex_data):
         if forex_data[i] < forex_data[i-1] and forex_data[i] < forex_data[i+1]:
             troughs.append(i)
     return troughs
+
+"""This function uses a for loop to iterate through the forex data. For each data point, 
+it checks if the current point is less than the point before it and the point after it. If both of those conditions are true, 
+then the current point is considered a trough and its index is added to the "troughs" list. Once the loop is finished, 
+the function returns the list of troughs.
+
+It's important to note that this function assumes that the forex data is a list or array of numerical values. 
+If the data is in a different format, the function will need to be modified accordingly."""
