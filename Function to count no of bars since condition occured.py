1dc56af8d063bcadabed22ecbfb5752350c49be6
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-22 19:25:56 +0300

    Update Function to count no of bars since condition occured.py

--- Function to count no of bars since condition occured.py
+++ Function to count no of bars since condition occured.py
@@ -8,3 +8,7 @@ def count_bars_since_condition(condition, data):
         elif condition_met:
             count += 1
     return count
+"""
+This function uses a for loop to iterate through the list of Forex bar data, and checks each bar against the provided condition. If the condition is met, the condition_met variable is set to True, and the loop continues. If the condition is not met and condition_met is True, the count is incremented. The function returns the final count.
+You can use this function by providing a specific condition and list of data as arguments, like this:v
+"""
