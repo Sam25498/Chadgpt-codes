5d91930e8facd1309defd178dc2bdc8d029677f4
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-01-22 19:25:30 +0300

    Update Function to count no of bars since condition occured.py

--- Function to count no of bars since condition occured.py
+++ Function to count no of bars since condition occured.py
@@ -1,1 +1,10 @@
 #ere is an example of a Python function that takes a condition and a list of Forex bar data as arguments, and outputs the number of bars since the last time the condition was true:ere is an example of a Python function that takes a condition and a list of Forex bar data as arguments, and outputs the number of bars since the last time the condition was true:
+def count_bars_since_condition(condition, data):
+    count = 0
+    condition_met = False
+    for bar in data:
+        if condition(bar):
+            condition_met = True
+        elif condition_met:
+            count += 1
+    return count
